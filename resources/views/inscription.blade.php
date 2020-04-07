5f8ca5fbb8982066fad870e39fc26e477bb0e1a0
Pierre Boislève <pierre.boisleve@gmail.com>
2020-04-07 17:34:05 +0200

    Initial Commit

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -3,17 +3,18 @@
 @section('contenu')
     <form>
         <div class="form-group">
-            <label for="nom">Nom</label>
-            <input type="text" class="form-control" id="nom" placeholder="Nom" required>
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-            <label for="prenom">Prénom</label>
-            <input type="text" class="form-control" id="prenom" placeholder="Prénom" required>
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
-        <div class="form-group">
-            <label for="prenom">Age</label>
-            <input type="number" class="form-control" id="age" value="20" min="20" max="80" required>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
-        <button class="btn" type="submit">Envoyer</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 @endsection
